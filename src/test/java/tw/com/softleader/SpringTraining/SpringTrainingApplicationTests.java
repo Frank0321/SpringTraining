d11cf40c3c48a849c80b3edff93bc82fd6666c4b
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-07-23 21:31:32 +0800

    建立專案

---
+++ src/test/java/tw/com/softleader/SpringTraining/SpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.softleader.SpringTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
